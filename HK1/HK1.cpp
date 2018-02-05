7ca554cf986f8b8ec8ac3818b226d3cdee91d328
DAMIAN HERNANDEZ <DAMHERNA@uat.edu>
2018-02-04 23:26:40 -0700

    hk

--- HK1/HK1.cpp
+++ HK1/HK1.cpp
@@ -2,10 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <ctime>
 
+using std::cout;
+using std::endl;
 
 int main()
 {
+	//Generate Random Enemy location
+	cout << "Enemy located in an 8x8 Grid..." << endl;
+	srand(time(0));
+
+	int searchHiNmbr = 64;
+	int searchLoNmbr = 1;
+
+	int EnemyLocation = rand() % searchHiNmbr + searchLoNmbr;
+	cout << "Beginning search for enemy!" << endl;
+
+	int targetLocaPrediCount = 0;
+	bool targetFound = false;
+
+	while (targetFound == false)
+	{
+		int targetLocaPredi = ((searchHiNmbr - searchLoNmbr) / 2) + searchLoNmbr;
+
+		targetLocaPrediCount++;
+
+		if (targetLocaPredi > EnemyLocation)
+		{
+			searchHiNmbr = targetLocaPredi - 1;
+			cout << "Too high" << endl;
+		}
+		else if (targetLocaPredi < EnemyLocation)
+		{
+			searchLoNmbr = targetLocaPredi + 1;
+			cout << "Too low" << endl;
+		}
+		else 
+		{
+			cout << "Search: " << targetLocaPrediCount << ". Target located! Beginning extermination!" << endl;
+			targetFound = true;
+		}
+	}
+
     return 0;
 }
 
